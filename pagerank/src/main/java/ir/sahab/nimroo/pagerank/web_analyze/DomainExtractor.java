891e385a29ccfdb4fcc597357926c038eb6a81ae
sina <taslimisina@yahoo.com>
2018-09-02 12:50:57 +0430

    Refactored DomainExtractor and KeywordRelationFinder

--- pagerank/src/main/java/ir/sahab/nimroo/pagerank/web_analyze/DomainExtractor.java
+++ pagerank/src/main/java/ir/sahab/nimroo/pagerank/web_analyze/DomainExtractor.java
@@ -1,17 +1,13 @@
 package ir.sahab.nimroo.pagerank.web_analyze;
 
-import ir.sahab.nimroo.Config;
+import ir.sahab.nimroo.pagerank.HBaseAPI;
 import ir.sahab.nimroo.util.LinkNormalizer;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.CellUtil;
-import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
-import org.apache.hadoop.hbase.mapreduce.TableInputFormat;
-import org.apache.hadoop.hbase.mapreduce.TableOutputFormat;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.log4j.Logger;
@@ -21,31 +17,33 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class DomainExtractor {
 	private static Logger logger = Logger.getLogger(DomainExtractor.class);
-	private static Configuration hBaseConfiguration = null;
+	private String inputTable, inputFamily, outputTable, outputFamily;
+	private JavaSparkContext javaSparkContext;
+	private HBaseAPI hBaseAPI;
 
-	public void extractDomain() {
-		Config.load();
+	public DomainExtractor(String inputTable, String inputFamily, String outputTable, String outputFamily) {
 		PropertyConfigurator.configure(DomainExtractor.class.getClassLoader().getResource("log4j.properties"));
+
+		this.inputTable = inputTable;
+		this.inputFamily = inputFamily;
+		this.outputTable = outputTable;
+		this.outputFamily = outputFamily;
 
 		SparkConf sparkConf = new SparkConf();
 		sparkConf.setAppName("Domain Extractor");
-		JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
+		javaSparkContext = new JavaSparkContext(sparkConf);
 
-		hBaseConfiguration = HBaseConfiguration.create();
-		hBaseConfiguration.set(TableInputFormat.INPUT_TABLE, ""); //todo
-		hBaseConfiguration.set(TableInputFormat.SCAN_COLUMN_FAMILY, ""); //todo
-		hBaseConfiguration.addResource(Config.hBaseSite);
-		hBaseConfiguration.addResource(Config.hadoopCoreSite);
+		hBaseAPI = new HBaseAPI();
+	}
 
+	public void extractDomain() {
 		JavaPairRDD<ImmutableBytesWritable, Result> hBaseRDD =
-				javaSparkContext.newAPIHadoopRDD(hBaseConfiguration, TableInputFormat.class,
-						ImmutableBytesWritable.class, Result.class);
+				hBaseAPI.getRDD(javaSparkContext, inputTable, inputFamily);
 
 		JavaPairRDD<Tuple2<String,String>, Integer> domainSinkDomain = hBaseRDD.flatMapToPair(pairRow -> {
 			Result result = pairRow._2;
@@ -78,26 +76,13 @@ public class DomainExtractor {
 			int edgeNumber = domainSinkDomainNum._2;
 
 			Put put = new Put(DigestUtils.md5Hex(domain).getBytes());
-			put.addColumn(Bytes.toBytes(""), Bytes.toBytes("domain"), Bytes.toBytes(domain));       //todo
-			put.addColumn(Bytes.toBytes(""), Bytes.toBytes(sinkDomain), Bytes.toBytes(edgeNumber));     //todo
+			put.addColumn(Bytes.toBytes(outputFamily), Bytes.toBytes("domain"), Bytes.toBytes(domain));
+			put.addColumn(Bytes.toBytes(outputFamily), Bytes.toBytes(sinkDomain), Bytes.toBytes(edgeNumber));
 
 			return new Tuple2<>(new ImmutableBytesWritable(), put);
 		});
 
-
-		Job job = null;
-		try {
-			job = Job.getInstance(hBaseConfiguration);
-			job.setOutputFormatClass(TableOutputFormat.class);
-			job.getConfiguration().set(TableOutputFormat.OUTPUT_TABLE, ""); //todo
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
+		Job job = hBaseAPI.getJob(outputTable);
 		hBasePuts.saveAsNewAPIHadoopDataset(job.getConfiguration());
 	}
 }
-
-
-
-
